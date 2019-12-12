1b68acd58e30c027d141e5aac078fb6a04352498
yankovs <49753115+yankovs@users.noreply.github.com>
2019-12-12 15:58:19 +0200

    Add files via upload

---
+++ Command.h
@@ -0,0 +1,14 @@
+//
+// Created by sharon on 12/11/19.
+//
+
+#include <string>
+
+#ifndef CPPEX3_COMMAND_H
+#define CPPEX3_COMMAND_H
+
+class Command {
+    virtual int execute(std::string);
+};
+
+#endif //CPPEX3_COMMAND_H
