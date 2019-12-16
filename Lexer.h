f2f16f976e718ddabb994e910cc4e03d4616627c
Liad Elinson <liad.elinson@gmail.com>
2019-12-16 22:05:28 +0200

    lexer update 16/12 (completed hopefully)

--- Lexer.h
+++ Lexer.h
@@ -10,120 +10,144 @@
 #include <fstream>
 #include <iostream>
 #include <cstring>
+#include <algorithm>
 
 class Lexer {
  public:
-  std::list<std::string> finalList{"Start"}; //finalList needs to be not empty at the start
+  std::list<std::string> finalList; //finalList needs to be not empty at the start
   void readFile(std::fstream &file) {
     std::string line;
-    std::string delimiter = " ";
-    size_t pos = 0;
-    std::string token;
     int flag;
     while (std::getline(file, line)) {
-      flag = 1;
-      //if we passed the start and there are strings in finalList, "Start" can be removed from there.
-      if(finalList.size() >= 2 && finalList.front() == "Start")
+      flag = 0;
+      line = clearSpaces(line);
+      if(line == "")
       {
-        finalList.erase(finalList.begin());
-      }
-      //ignoring tabs or spaces at the start of line:
-      while (line.substr(0, 1) == "\t" || line.substr(0, 1) == " ") {
-        line.erase(0, 1);
-      }
-      //if the line looks like "Print("...")" we don't need the brackets
-      if (line.substr(0, 5) == "Print") {
-        finalList.insert(finalList.end(), "Print");
-        line.erase(0, 6);
-        line.replace(line.length() - 1, 1, "");
-        finalList.insert(finalList.end(), line);
         continue;
       }
-        //if the line starts with var
+      if (line.substr(0, 20) == "connectControlClient" || line.substr(0, 5) == "Sleep" ||
+               line.substr(0, 14) == "openDataServer" || line.substr(0, 5) == "Print") {
+        flag = 1;
+      }
+      //if the line starts with var
       else if (line.substr(0, 3) == "var") {
         finalList.insert(finalList.end(), "var");
         line.erase(0, 4);
-        //in case there are another spaces or tabs after "var"
-        while (line.substr(0, 1) == "\t" || line.substr(0, 1) == " ") {
-          line.erase(0, 1);
-        }
+        line = clearSpaces(line);
         for (int i = 0; i < line.length(); i++) {
           if (line.at(i) == '-' && line.at(i + 1) == '>') {
             finalList.insert(finalList.end(), line.substr(0, i));
             line.erase(0, i);
             finalList.insert(finalList.end(), "->");
             line.erase(0, 2);
-            while (line.substr(0, 1) == "\t" || line.substr(0, 1) == " ") {
-              line.erase(0, 1);
-            }
+            line = clearSpaces(line);
+            flag = 1;
             continue;
           } else if (line.at(i) == '<' && line.at(i + 1) == '-') {
             finalList.insert(finalList.end(), line.substr(0, i));
             line.erase(0, i);
             finalList.insert(finalList.end(), "<-");
             line.erase(0, 2);
-            while (line.substr(0, 1) == "\t" || line.substr(0, 1) == " ") {
-              line.erase(0, 1);
-            }
+            line = clearSpaces(line);
+            flag = 1;
             continue;
           }
-            //if line looks like: "var x = ..." where x is the variable and ... is an expression
+          //if line looks like: "var x = ..." where x is the variable and ... is an expression
           else if (line.at(i) == '=') {
             finalList.insert(finalList.end(), line.substr(0, i));
             line.erase(0, i);
             finalList.insert(finalList.end(), "=");
             line.erase(0, 1);
-            while (line.substr(0, 1) == "\t" || line.substr(0, 1) == " ") {
-              line.erase(0, 1);
-            }
+            line = clearSpaces(line);
+            line = dealWithExpressions(line);
             finalList.insert(finalList.end(), line);
-            line.erase(0, line.length());
-            flag = 0;
             continue;
-          } else if (line.at(i) == ' ' || line.at(i) == '\t') {
-            finalList.insert(finalList.end(), line.substr(0, i));
-            line.erase(0, i);
-            while (line.substr(0, 1) == "\t" || line.substr(0, 1) == " ") {
-              line.erase(0, 1);
-            }
-            //assuming after name of variable only "->", "<-" or "=" can appear
-            if (line.at(0) == '-' && line.at(1) == '>') {
-              finalList.insert(finalList.end(), "->");
-              line.erase(0, 2);
-              while (line.substr(0, 1) == "\t" || line.substr(0, 1) == " ") {
-                line.erase(0, 1);
-              }
-              continue;
-            } else if (line.at(0) == '<' && line.at(1) == '-') {
-              finalList.insert(finalList.end(), "<-");
-              line.erase(0, 2);
-              while (line.substr(0, 1) == "\t" || line.substr(0, 1) == " ") {
-                line.erase(0, 1);
-              }
-              continue;
-            } else if (line.at(0) == '=') {
-              finalList.insert(finalList.end(), "=");
-              line.erase(0, 1);
-              while (line.substr(0, 1) == "\t" || line.substr(0, 1) == " ") {
-                line.erase(0, 1);
-              }
-              finalList.insert(finalList.end(), line);
-              line.erase(0, line.length());
-              flag = 0;
-              continue;
-            }
           }
         }
+        //if the line looks like "var x = ..." it's possible to continue to the next line
+        if (!flag) {
+          continue;
+        }
       }//if the line starts with var
-      if (flag || line.substr(0, 3) == "sim" || line.substr(0, 14) == "openDataServer" ||
-          line.substr(0, 20) == "connectControlClient" || line.substr(0, 5) == "Sleep") {
-        //the tokens are separated by delimiter which is " "
-        /*while ((pos = line.find(delimiter)) != std::string::npos) {
-          token = line.substr(0, pos);
-          parseString(finalList, finalList.end(), token);
-          line.erase(0, pos + delimiter.length());
-        }*/
-        finalList.end(), parseString(finalList, finalList.end(), line);
+
+      //if the line starts with while or if
+      else if (line.substr(0, 5) == "while" || line.substr(0, 2) == "if") {
+        if (line.substr(0, 5) == "while") {
+          finalList.insert(finalList.end(), "while");
+          line.erase(0, 6);
+        } else {
+          //if at the start of line
+          finalList.insert(finalList.end(), "if");
+          line.erase(0, 3);
+        }
+        line = clearSpaces(line);
+        //assuming the line looks like "while/if condition {"
+        line = line.substr(0, line.length() - 1);
+        line = clearSpaces(line);
+        line = dealWithExpressions(line);
+        finalList.insert(finalList.end(), line);
+        finalList.insert(finalList.end(), "{");
+        continue;
+      }
+      else if (line == "}") {
+        finalList.insert(finalList.end(), line);
+        continue;
+      }
+      //if flag == 1 it means the line currently looks like something(...)
+      //assumption: there are no variables that their name is something like "PrintABC"
+      if(flag)
+      {
+        int count = 0;
+        while (line[count] != '(') {
+          count++;
+        }
+        std::string partOne = line.substr(0, count);
+        finalList.insert(finalList.end(), partOne);
+        line.erase(0, count + 1);
+        line = clearSpaces(line);
+        line.replace(line.length() - 1, 1, "");
+        if (partOne == "connectControlClient") {
+          int count2 = 1;
+          while (line[count2] != '"') {
+            count2++;
+          }
+          finalList.insert(finalList.end(), line.substr(0, count2 + 1));
+          line.erase(0, count2 + 1);
+          while (line.substr(0, 1) == "\t" || line.substr(0, 1) == " " ||
+              line.substr(0, 1) == ",") {
+            line.erase(0, 1);
+          }
+          line = dealWithExpressions(line);
+          finalList.insert(finalList.end(), line);
+        }
+        else
+        {
+          //partOne is either sim, Print, openDataServer or Sleep in this case
+          if(line.at(0) != '"')
+          {
+            line = dealWithExpressions(line);
+          }
+          finalList.insert(finalList.end(), line);
+        }
+        continue;
+      }
+      //in this case the line looks like "variable = expression"
+      else
+      {
+        int index = 0;
+        while (line[index] != '=' && line[index] != ' ' && line[index] != '\t') {
+          index++;
+        }
+        finalList.insert(finalList.end(), line.substr(0, index));
+        line.erase(0, index);
+        finalList.insert(finalList.end(), "=");
+        while (line.substr(0, 1) == "\t" || line.substr(0, 1) == " " ||
+            line.substr(0, 1) == "=") {
+          line.erase(0, 1);
+        }
+        line = dealWithExpressions(line);
+        finalList.insert(finalList.end(), line);
+        continue;
       }
     }
   }
@@ -134,63 +158,27 @@ class Lexer {
     }
   }
 
-  //dealing with lines like open openDataServer where we dont want brackets to be in final list
-  void parseString(std::list<std::string> strList, std::list<std::string>::iterator it, std::string stringToParse) {
-    if (stringToParse[stringToParse.length() - 1] == ')') {
-      int count = 0;
-      while (stringToParse[count] != '(') {
-        count++;
-      }
-      std::string partOne = stringToParse.substr(0, count);
-      strList.insert(it, partOne);
-      stringToParse.erase(0, count + 1);
-      stringToParse.replace(stringToParse.length() - 1, 1, "");
-      while (stringToParse.substr(0, 1) == "\t" || stringToParse.substr(0, 1) == " ") {
-        stringToParse.erase(0, 1);
-      }
-      if (partOne == "sim") {
-        strList.insert(it, stringToParse);
-        stringToParse.erase(0, stringToParse.length());
-      } else if (partOne == "openDataServer" || partOne == "Sleep") {
-        strList.insert(it, stringToParse);
-        stringToParse.erase(0, stringToParse.length());
-      } else if (partOne == "connectControlClient") {
-        int count2 = 1;
-        while (stringToParse[count2] != '"') {
-          count2++;
-        }
-        strList.insert(it, stringToParse.substr(0, count2 + 1));
-        stringToParse.erase(0, count2 + 1);
-        while (stringToParse.substr(0, 1) == "\t" || stringToParse.substr(0, 1) == " " ||
-            stringToParse.substr(0, 1) == ",") {
-          stringToParse.erase(0, 1);
-        }
-        strList.insert(it, stringToParse);
-        stringToParse.erase(0, stringToParse.length());
-      }
+  //function that clears spaces and tabs from the start and end of the string it gets as a parameter.
+  std::string clearSpaces(std::string stringToBeCleared) {
+    //in case there are useless spaces or tabs at the start of string
+    while (stringToBeCleared.substr(0, 1) == "\t" || stringToBeCleared.substr(0, 1) == " ") {
+      stringToBeCleared.erase(0, 1);
     }
-    //in this case the line looks like "variable = expression"
-    else if(stringToParse.substr(0,2) != "if" && stringToParse.substr(0,5) != "while" &&
-            stringToParse != "}")
-    {
-      int index = 0;
-      while (stringToParse[index] != '=' && stringToParse[index] != ' ' && stringToParse[index] != '\t') {
-        index++;
-      }
-      strList.insert(it, stringToParse.substr(0, index));
-      stringToParse.erase(0, index);
-      strList.insert(it, "=");
-      while (stringToParse.substr(0, 1) == "\t" || stringToParse.substr(0, 1) == " " ||
-          stringToParse.substr(0, 1) == "=") {
-        stringToParse.erase(0, 1);
-      }
-      strList.insert(it, stringToParse);
+    //in case there are useless spaces or tabs at the end of string
+    while (stringToBeCleared.back() == '\t' || stringToBeCleared.back() == ' ') {
+      stringToBeCleared = stringToBeCleared.substr(0, stringToBeCleared.length() - 1);
     }
+    return stringToBeCleared;
+  }
 
-    else {
-      strList.insert(it, stringToParse);
-    }
-
+  //function that clears spaces and tabs that are in an expression
+  std::string dealWithExpressions(std::string exp)
+  {
+    exp = clearSpaces(exp);
+    //solution found from https://www.geeksforgeeks.org/remove-spaces-from-a-given-string/
+    exp.erase(std::remove(exp.begin(), exp.end(), ' '), exp.end());
+    exp.erase(std::remove(exp.begin(), exp.end(), '\t'), exp.end());
+    return exp;
   }
 };
 
